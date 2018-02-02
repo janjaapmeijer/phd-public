d59a1088114f2335c1d3a3d4cc4d04b576afc9cb
Jan Jaap Meijer <janjaapmeijer@gmail.com>
2018-02-02 12:20:08 +1100

    Let python2 read the paths too.

--- _setup.py
+++ _setup.py
@@ -7,7 +7,7 @@ if sys.platform == 'darwin':
     datadir = os.path.join(phddir, 'data')
     srcdir = os.path.join(phddir, 'src')
 
-elif sys.platform == 'linux':
+elif sys.platform == 'linux' or sys.platform == 'linux2':
     homedir = os.path.abspath(os.path.join(os.sep, 'home', 'janjaapmeijer'))
     phddir = os.path.join(homedir, 'Checkouts', 'PhD')
     datadir = os.path.join(phddir, 'data')
