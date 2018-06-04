aa41de08297afa4cad473d4229e320fccd8997fa
Jan Jaap Meijer <janjaapmeijer@gmail.com>
2018-06-04 17:15:09 +1000

    Adding outdir to setup script.

--- _setup.py
+++ _setup.py
@@ -6,9 +6,11 @@ if sys.platform == 'darwin':
     phddir = os.path.join(homedir, 'Checkouts', 'PhD')
     datadir = os.path.join(phddir, 'data')
     srcdir = os.path.join(phddir, 'src')
+    outdir = os.path.join(phddir, 'out')
 
 elif sys.platform == 'linux' or sys.platform == 'linux2':
     homedir = os.path.abspath(os.path.join(os.sep, 'home', 'janjaapmeijer'))
     phddir = os.path.join(homedir, 'Checkouts', 'PhD')
     datadir = os.path.join(phddir, 'data')
     srcdir = os.path.join(phddir, 'src')
+    outdir = os.path.join(phddir, 'out')
